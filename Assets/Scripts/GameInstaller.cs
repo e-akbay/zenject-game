efd42b006f815b4426692f420cbd2453e35a6606
e-akbay <emirakbay99@gmail.com>
2025-06-14 00:35:37 +0300

    basic character controller implemented

---
+++ Assets/Scripts/GameInstaller.cs
@@ -0,0 +1,9 @@
+﻿using Zenject;
+
+public class GameInstaller : MonoInstaller<GameInstaller>
+{
+    public override void InstallBindings()
+    {
+        Container.BindInterfacesAndSelfTo<InputReader.InputReader>().AsSingle();
+    }
+}
